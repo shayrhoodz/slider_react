ff4752cb5eb0df6c708fc46551972a2d39c30e98
Radik Shaymardanov <kaddafi88@gmail.com>
2018-12-15 00:01:56 +0500

    Создание приложения 1 день

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="wrapper">
+      <SaveFullName name='Radik' surname='Shaymardanov' link="vk.com" />
+      <SaveFullName name='Juilua' surname='Shaymardanova' link="facebook.com" />
+      <SaveFullName name='Rad' surname='Shaymardanov' link="#" />
+    </div>
     );
   }
 }
+
+function SaveFullName(props) {
+  return(
+    <div>
+      <h1> Мое имя {props.name}, фамилия - {props.surname} </h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+    </div>
+  )
+}
+
 
 export default App;
